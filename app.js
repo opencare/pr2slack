3eab1a99c1be490e946a7d926ffe036ae532ab58
nivivon <niv@opencare.com>
2016-08-18 13:02:24 -0400

    made PR fixes and pointed Profile to app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var express = require('express');
 var logfmt = require('logfmt');
 var request = require('request');
+var _ = require('lodash');
 var Slack = require('slack-node');
 
 var missing = [];
 
-const slackBotUsername = 'github';
-const slackBotIconURL = 'https://slack-assets2.s3-us-west-2.amazonaws.com/10562/img/services/github_48.png';
+var slackBotUsername = 'github';
+var slackBotIconURL = 'https://slack-assets2.s3-us-west-2.amazonaws.com/10562/img/services/github_48.png';
 
-for (var key in ['USERNAME', 'PASSWORD', 'SLACK_WEBHOOK_URI', 'SLACK_RELEASE_WEBHOOK_URI']) {
-  if (!key in process.env) {
-    missing.push(key);
-  }
-}
-
-if (missing.length) {
-  throw "Missing environment variables: " + (missing.join(', '));
+var requiredEnvVars = ['USERNAME', 'PASSWORD', 'SLACK_WEBHOOK_URI', 'SLACK_RELEASE_WEBHOOK_URI', 'SLACK_API_TOKEN'];
+if (_.intersection(_.keys(process.env), requiredVars).length != requiredVars.length) {
+  throw 'Missing environment variables: ' + (missing.join(', '));
 }
 
 var app = express();
@@ -28,6 +24,21 @@ app.use(express.basicAuth(function(username, password) {
 
 app.use(express.bodyParser());
 
+var apiToken = process.env.SLACK_API_TOKEN;
+
+var slackWebhook = new Slack();
+var slackAPI = new Slack(apiToken);
+
+var githubToSlack = {
+  'camthesixth': 'cam',
+  'davidjconnolly': 'dave',
+  'leeopencare': 'lee',
+  'paulfeltoe': 'paul',
+  'nivivon': 'nivivon',
+  'RonenA': 'ronen',
+  'vadim-zverugo': 'vadim'
+};
+
 var unescaped = /[&<>]/;
 
 // Can't use underscore's escape because quotes are already escaped in Slack by
@@ -38,13 +49,7 @@ var htmlEscapes = {
   '>': '&gt;'
 };
 
-var htmlEscapesKeys = [];
-for (var key in htmlEscapes) {
-  if (htmlEscapes.hasOwnProperty(key)) {
-    htmlEscapesKeys.push(key);
-  }
-}
-
+var htmlEscapesKeys = _.keys(htmlEscapes);
 var reUnescapedHtml = new RegExp(htmlEscapesKeys.join(''),'g');
 
 var escapeHtmlChar = function(match) {
@@ -88,13 +93,13 @@ app.post('/', function(req, res) {
     var prDataExists = payload.pull_request && payload.pull_request.base;
     if (prDataExists) {
       if (prCreated && payload.pull_request.base.ref != 'production') {
-        text = ":rocket: " + payload.pull_request.user.login + " " + payload.action + " a pull request in <" + payload.pull_request.base.repo.html_url + "|" + (escape(payload.pull_request.base.repo.name)) + ">\n*<" + payload.pull_request.html_url + "|" + (escape(payload.pull_request.title)) + ">*";
+        text = ':rocket: ' + payload.pull_request.user.login + ' ' + payload.action + ' a pull request in <' + payload.pull_request.base.repo.html_url + '|' + (escape(payload.pull_request.base.repo.name)) + '>\n*<' + payload.pull_request.html_url + '|' + (escape(payload.pull_request.title)) + '>*';
       }
       else if (releaseMerged && payload.action == 'closed' && payload.pull_request.merged && payload.pull_request.base.ref == 'production') {
         uri = process.env.SLACK_RELEASE_WEBHOOK_URI;
-        text = ":robot_face: " + payload.pull_request.user.login + " released <" + payload.pull_request.base.repo.html_url + "|" + (escape(payload.pull_request.base.repo.name)) + ">\n\n";
-        text += "*<" + payload.pull_request.html_url + "|" + (escape(payload.pull_request.title)) + ">*\n\n";
-        body = ("" + (escape(payload.pull_request.body))).replace(/\[#(\d+)\]/g, "<https://www.pivotaltracker.com/story/show/$1|[#$1]>");
+        text = ':robot_face: ' + payload.pull_request.user.login + ' released <' + payload.pull_request.base.repo.html_url + '|' + (escape(payload.pull_request.base.repo.name)) + '>\n\n';
+        text += '*<' + payload.pull_request.html_url + '|' + (escape(payload.pull_request.title)) + '>*\n\n';
+        body = ('' + (escape(payload.pull_request.body))).replace(/\[#(\d+)\]/g, '<https://www.pivotaltracker.com/story/show/$1|[#$1]>');
         text += body;
       }
     }
@@ -106,47 +111,31 @@ app.post('/', function(req, res) {
     // Shipit given
     if (shipitGiven) {
       resType = ResType.Webhook;
-      text = payload.comment.body + " from " + payload.comment.user.login + "! on a PR in <" + payload.repository.html_url + "|" + payload.repository.name + ">\n<" + payload.issue.html_url + "|" + payload.issue.title + ">";
+      text = payload.comment.body + ' from ' + payload.comment.user.login + '! on a PR in <' + payload.repository.html_url + '|' + payload.repository.name + '>\n<' + payload.issue.html_url + '|' + payload.issue.title + '>';
     }
     // Feedback given
     else {
       resType = ResType.API;
-      var githubToSlack = {
-        "camthesixth": "cam",
-        "davidjconnolly": "dave",
-        "leeopencare": "lee",
-        "paulfeltoe": "paul",
-        "nivivon": "nivivon",
-        "RonenA": "ronen",
-        "vadim-zverugo": "vadim"
-      };
 
       if (payload.comment.user.login in pivotalToSlack) {
         username = githubToSlack[payload.issue.user.login];
-        text = payload.comment.user.login + " commented on your PR in <" + payload.repository.html_url + "|" + payload.repository.name + ">: " + payload.comment.body + "\n<" + payload.issue.html_url + "|" + payload.issue.title + ">";
+        text = payload.comment.user.login + ' commented on your PR in <' + payload.repository.html_url + '|' + payload.repository.name + '>: ' + payload.comment.body + '\n<' + payload.issue.html_url + '|' + payload.issue.title + '>';
       }
     }
   }
 
-  if (text == null) {
-    return res.json(200);
-  }
-
   if (resType == ResType.Webhook) {
-    var slack = new Slack();
-    slack.setWebhook(uri);
-    slack.webhook({
+    slackWebhook.setWebhook(uri);
+    slackWebhook.webhook({
       text: text,
       username: slackBotUsername,
       icon_url: slackBotIconURL
     }, handleResponse);
   }
   else if (resType == ResType.API) {
-    var apiToken = process.env.SLACK_API_TOKEN;
-    var slack = new Slack(apiToken);
-    slack.api('chat.postMessage', {
+    slackAPI.api('chat.postMessage', {
       text:text,
-      channel:"@" + username,
+      channel:'@' + username,
       username: slackBotUsername,
       icon_url: slackBotIconURL
    }, handleResponse);
@@ -156,5 +145,5 @@ app.post('/', function(req, res) {
 port = !!process.env.PORT ? process.env.PORT : 5000;
 
 app.listen(port, function() {
-  return console.log("Listening on " + port);
+  return console.log('Listening on ' + port);
 });
